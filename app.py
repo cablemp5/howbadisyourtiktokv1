a449d45d442c63bf05bcb92478391cdd3b3e1dc4
cablemp5 <cmikec12@gmail.com>
2025-02-22 00:05:16 -0500

    Visual update

--- app.py
+++ app.py
@@ -3,40 +3,35 @@ import random
 import re
 import time
 import zipfile
-from concurrent.futures import as_completed
-from io import StringIO, BytesIO
-
 import pandas as pd
 import streamlit as st
+from concurrent.futures import as_completed
+from io import StringIO, BytesIO
 from google import genai
 from requests_futures.sessions import FuturesSession
 
-NUM_LINKS_TO_PARSE = 0
-NUM_HASHTAGS_TO_ANALYZE = 0
-NUM_TO_SHOW = 0
-
-API_KEY = ""
+num_links_to_parse = 0
+num_links_to_analyze = 0
+num_items_to_show = 0
 
 TAG_IGNORE = ["#fyp", "#viral", "#foryou", "fy", "#foryoupage", "#trending",
               "#fyp", "#fypシ゚viral", "#fypシ", "#blowthisup",
               "#fyppppppppppppppppppppppp", "#fy", "#fypage", "#viralvideo",
               "#xyzbca"]
 
+def gemini_analysis(payload):
 
-def gemini_analysis(dictionary):
-  client = genai.Client(api_key=API_KEY)
-
+  client = genai.Client(api_key=st.secrets.key)
   prompt = st.secrets.prompt
 
   with (st.spinner('Loading AI analysis...')):
     try:
       response = client.models.generate_content(model="gemini-2.0-flash",
                                                 contents=[
-                                                  prompt + str(dictionary)])
+                                                  prompt + str(payload)])
       return response.text
     except Exception as e:
-      return "Error: " + str(e)
-
+      return e
 
 def parse_tiktok_links(zip_file, selection):
   with zipfile.ZipFile(zip_file, 'r') as z:
@@ -44,8 +39,10 @@ def parse_tiktok_links(zip_file, selection):
       html_data = json.load(f)
 
   links = []
-  liked_videos = [i["link"] for i in html_data["Activity"]["Like List"]["ItemFavoriteList"]]
-  saved_videos = [i["Link"] for i in html_data["Activity"]["Favorite Videos"]["FavoriteVideoList"]]
+  liked_videos = [i["link"] for i in
+                  html_data["Activity"]["Like List"]["ItemFavoriteList"]]
+  saved_videos = [i["Link"] for i in
+                  html_data["Activity"]["Favorite Videos"]["FavoriteVideoList"]]
 
   if "Liked" in selection and "Saved" in selection:
     while liked_videos or saved_videos:
@@ -60,7 +57,6 @@ def parse_tiktok_links(zip_file, selection):
 
   return list(set(links))
 
-
 def sort_dict(dictionary):
   sorted_dict = (
     dict(sorted(dictionary.items(), key=lambda x: x[1], reverse=True)))
@@ -70,19 +66,19 @@ def sort_dict(dictionary):
 
   return sorted_dict
 
-
 def scrape_tiktok(links, st_progress_bar):
+
+  description_list = []
   hashtag_dict = {}
   username_dict = {}
-  description_list = []
-  pfp_dict = {}
+  avatar_dict = {}
 
-  num_to_scrape = min(NUM_LINKS_TO_PARSE, len(links))
+  num_to_scrape = min(num_links_to_parse, len(links))
   num_scraped = 0
-  start_time = time.time()  # Track the start time
+  start_time = time.time()
 
   with FuturesSession() as session:
-    futures = [session.get(links[i]) for i in range(min(NUM_LINKS_TO_PARSE, len(links)))]
+    futures = [session.get(links[i]) for i in range(min(num_links_to_parse, len(links)))]
 
     for future in as_completed(futures):
       response = future.result()
@@ -91,15 +87,13 @@ def scrape_tiktok(links, st_progress_bar):
       html = response.text
 
       num_scraped += 1
-      elapsed_time = time.time() - start_time  # Calculate elapsed time
-      avg_time_per_video = elapsed_time / num_scraped  # Calculate average time per video
-      remaining_time = (num_to_scrape - num_scraped) * avg_time_per_video  # Estimate remaining time
+      elapsed_time = time.time() - start_time
+      avg_time_per_video = elapsed_time / num_scraped
+      remaining_time = (num_to_scrape - num_scraped) * avg_time_per_video
 
-      # Convert remaining time to minutes and seconds
       eta_minutes = int(remaining_time // 60)
       eta_seconds = int(remaining_time % 60)
 
-      # Update progress bar with percentage and ETA
       st_progress_bar.progress(num_scraped / num_to_scrape,
                                text=f"**{num_scraped}/{num_to_scrape} videos scanned...  |   ⏳ Time Remaining - {eta_minutes:02d}:{eta_seconds:02d}**")
 
@@ -110,20 +104,20 @@ def scrape_tiktok(links, st_progress_bar):
         description = description.group(1)
       description_list.append(description)
 
-      names = re.search('\"uniqueId\":\"([^\"]+)\",\"nickname\":\"([^\"]+)\"', html)
-      if not names:  # Creator has no name
-        name = "Name Unavailable"
+      username = re.search('\"uniqueId\":\"([^\"]+)\",\"nickname\":\"([^\"]+)\"', html)
+      if not username:
+        username = "Name Unavailable"
       else:
-        name = "@" + names.group(1)
-      username_dict.setdefault(name, 0)
-      username_dict[name] += 1
+        username = "@" + username.group(1)
+      username_dict.setdefault(username, 0)
+      username_dict[username] += 1
 
-      pfp = re.search('\"avatarMedium\":\"([^\"]+)\"', html)
-      if not pfp:
-        pfp_link = ""
+      avatar = re.search('\"avatarMedium\":\"([^\"]+)\"', html)
+      if not avatar:
+        avatar = ""
       else:
-        pfp_link = pfp.group(1).replace("\\u002F", "/")
-      pfp_dict[name] = pfp_link
+        avatar = avatar.group(1).replace("\\u002F", "/")
+      avatar_dict[username] = avatar
 
       hashtags = re.findall("#\S+", description)
       for tag in hashtags:
@@ -134,30 +128,35 @@ def scrape_tiktok(links, st_progress_bar):
   time.sleep(0.5)
   st_progress_bar.empty()
 
-  return hashtag_dict, username_dict, description_list, pfp_dict
-
+  return description_list, hashtag_dict, username_dict, avatar_dict
 
 def on_upload(zip_file, selection, st_progress_bar):
-  links = parse_tiktok_links(zip_file,selection)
-  hashtag_dict, usernames_dict, description_list, pfp_dict = scrape_tiktok(
-      links,
-      st_progress_bar)
+  links = parse_tiktok_links(zip_file, selection)
+
+  description_list, hashtag_dict, username_dict, avatar_dict = scrape_tiktok(
+      links, st_progress_bar)
 
   sorted_hashtags = sort_dict(hashtag_dict)
+  sorted_users = sort_dict(username_dict)
 
-  sorted_users = sort_dict(usernames_dict)
   if "Name Unavailable" in sorted_users:
     del sorted_users["Name Unavailable"]
 
-  prompt_addition = str(dict(list(sorted_hashtags.items())[
-                             0:min(NUM_HASHTAGS_TO_ANALYZE,
-                                   len(sorted_hashtags))])) + str(
-      description_list)
-  return dict(
-      list(sorted_users.items())[0:min(NUM_TO_SHOW, len(sorted_users))]), dict(
-      list(sorted_hashtags.items())[
-      0:NUM_TO_SHOW]), pfp_dict, gemini_analysis(prompt_addition)
+  num_hashtags_to_show = min(num_items_to_show, len(sorted_hashtags))
+  top_hashtags_to_show = dict(list(sorted_hashtags.items())[:num_hashtags_to_show])
 
+  num_users_to_show = min(num_items_to_show, len(sorted_users))
+  top_users = dict(list(sorted_users.items())[:num_users_to_show])
+
+  num_hashtags_to_analyze = min(num_links_to_analyze, len(sorted_hashtags))
+  top_hashtags_to_analyze = dict(list(sorted_hashtags.items())[:num_hashtags_to_analyze])
+
+  description_str = str(description_list)
+  prompt = str(top_hashtags_to_analyze) + description_str
+
+  gemini_result = gemini_analysis(prompt)
+
+  return top_users, top_hashtags_to_show, avatar_dict, gemini_result
 
 def stream_data(text):
   for word in text.split(" "):
@@ -169,7 +168,6 @@ def update_checkbox():
   st.session_state.show_text_input = not st.session_state.checkbox_state
   st.rerun()
 
-
 if "show_text_input" not in st.session_state:
   st.session_state.show_text_input = True
 if "checkbox_state" not in st.session_state:
@@ -177,28 +175,29 @@ if "checkbox_state" not in st.session_state:
 
 st.set_page_config(
     page_title="How bad is your TikTok?",
-    page_icon="🧠",
+    page_icon="https://i.postimg.cc/BnLW2WLk/favicon.png",
 )
 
-st.image("https://i.postimg.cc/cJpQBxrY/mylogo-1.png", width=100)
+st.image("https://i.postimg.cc/BnLW2WLk/favicon.png", width=100)
 st.title("How Bad Is Your Tiktok?")
 st.write(
     'Curious what **AI** really thinks of your TikTok feed? This self-proclaimed "content-connoisseur" will dive into your liked videos and provide a brutally honest—*and painfully accurate*—assessment of your digital footprint.')
 st.info(
-    "This project was heavily inspired by [How Bad Is Your Streaming Music?](https://pudding.cool/2021/10/judge-my-music/) by The Pudding!",
+    "This project was heavily inspired by [**How Bad Is Your Streaming Music?**](https://pudding.cool/2021/10/judge-my-music/) by The Pudding!",
     icon="✨")
 st.warning(
-    "By using this website, you acknowledge that analyzing your TikTok activity requires uploading your TikTok data and, optionally, providing a Gemini API key. All data remains private and is neither stored nor viewed by anyone other than you and Google's Gemini AI. For your security, please avoid including any personally identifiable information in the data you upload. Additionally, if you choose to use a Gemini API key, we recommend generating a new key that is not linked to any other projects to ensure the integrity and security of your data.",
+    "By using this website, you acknowledge that analyzing your TikTok activity requires uploading your TikTok data and, optionally, providing a Gemini API key. **All data remains private and is neither stored nor viewed by anyone other than you and Google's Gemini AI**. For your security, please avoid including any personally identifiable information in the data you upload. Additionally, if you choose to use a Gemini API key, we recommend generating a new key that is not linked to any other projects to ensure the integrity and security of your data.",
     icon='⚠️')
 st.success(
-    "   If you are on mobile, turn on wide mode located in the settings in the top right for the best user experience!",
+    "If you are on mobile, turn on **Wide Mode** located in the settings in the top right (**⋮**) for the best user experience!",
     icon='👀')
 
 with st.container(border=True):
   st.write("**📂  How To Download And Analyze Your TikTok Data:**")
   st.caption(
       "**1.** Follow the official [TikTok data request guide](https://support.tiktok.com/en/account-and-privacy/personalized-ads-and-data/requesting-your-data) to download your data  \n  **2.** When prompted, ensure you select the following settings using the reference images below for guidance:")
-  st.image("https://i.postimg.cc/HL2D6k0R/darkinstructionsrounded.png", use_container_width=True)
+  st.image("https://i.postimg.cc/HL2D6k0R/darkinstructionsrounded.png",
+           use_container_width=True)
   st.caption(
       '**3.** Upload the `TikTok_Data_XXXXXXXXXX.zip` file below and click **"🚀 Analyze Your Algorithm"** to start the analysis')
 
@@ -208,8 +207,10 @@ with st.expander("**Additional Help**", icon="💡"):
   st.write("**Generating a Gemini API key:**")
   st.caption(
       "1. Sign in with your Google account and create and your Gemini API key [here](https://aistudio.google.com/app/apikey)  \n  2. After signing in, select the option to use the Gemini API and accept the terms and conditions  \n  3. Generate your API key and copy it, using the reference images below for guidance")
-  st.image("https://i.postimg.cc/d07NyQHG/inst1rounded.png", use_container_width=True)
-  st.image("https://i.postimg.cc/cCcFSvCG/inst2rounded.png", use_container_width=True)
+  st.image("https://i.postimg.cc/d07NyQHG/inst1rounded.png",
+           use_container_width=True)
+  st.image("https://i.postimg.cc/cCcFSvCG/inst2rounded.png",
+           use_container_width=True)
   st.caption(
       '4. Toggle **"Use the testing/demo Gemini API key"** to off and paste the copied API key into the text input field labeled **"Enter your Gemini API Key:"**')
 
@@ -234,15 +235,14 @@ with st.container(border=True):
         label="**Enter your Gemini API Key:**",
         placeholder="e.g. AIsaSyDLyhv4ImdpQAqd8H_R54oj56VGdVFvnPK",
         help="Check the Help section to learn how to generate an API key"
-
     )
 
   selection = st.pills(
-        "**Select the type of videos to scan**:",
-        options=["Saved","Liked"],
-        default=["Saved"],
-        selection_mode='multi',
-        help="Most accurate with only **Saved** videos. Use **Liked** when there isn't a large amount of saved videos to scan"
+      "**Select the type of videos to scan**:",
+      options=["Saved", "Liked"],
+      default=["Saved"],
+      selection_mode='multi',
+      help="Most accurate with only **Saved** videos. Use **Liked** when there isn't a large amount of saved videos to scan"
   )
 
   st.session_state.uploaded_file = st.file_uploader(
@@ -251,9 +251,9 @@ with st.container(border=True):
       help="Check the Help section to learn how to download the correct data",
   )
 
-
-  col1, col2 = st.columns([0.65,0.35],gap="small")
-  with col2.popover("**Additional Settings**", icon="⚙️",use_container_width=True):
+  col1, col2 = st.columns([0.65, 0.35], gap="small")
+  with col2.popover("**Additional Settings**", icon="⚙️",
+                    use_container_width=True):
     st.markdown(
         """
         <style>
@@ -270,14 +270,18 @@ with st.container(border=True):
         """,
         unsafe_allow_html=True
     )
-    to_parse = st.slider("**📺 Number of videos to scan**:", min_value=1, max_value=10000, value=5000,
+    to_parse = st.slider("**📺 Videos to scan**:", min_value=1,
+                         max_value=10000, value=5000,
                          help="Default: 5000 videos")
-    to_analyze = st.slider("**📝 Number of hashtags to scan:**", min_value=10, max_value=500, value=500,
+    to_analyze = st.slider("**📝 Hashtags to scan:**", min_value=10,
+                           max_value=500, value=500,
                            help="Default: all hashtags (up to 500)")
-    num_to_show = st.slider("**👁️‍🗨️ Number of items to preview:**", min_value=3, max_value=100, value=25,
-                            help="Default: 25 hashtags/users (up to 100)")
+    to_show = st.slider("**👁️‍🗨️ Items to preview:**",
+                        min_value=3, max_value=100, value=25,
+                        help="Default: 25 hashtags/users (up to 100)")
 
-  if col1.button(label="**🚀 Analyze your algorithm**", use_container_width=True, type="secondary"):
+  if col1.button(label="**🚀 Analyze your algorithm**", use_container_width=True,
+                 type="secondary"):
     if not st.session_state.testKey and not st.session_state.key:
       st.error("**💥 You haven't added a Gemini API key!**")
     elif st.session_state.uploaded_file is None:
@@ -287,42 +291,50 @@ with st.container(border=True):
     else:
       zip_file = BytesIO(st.session_state.uploaded_file.getvalue())
 
-      NUM_LINKS_TO_PARSE = to_parse
-      NUM_HASHTAGS_TO_ANALYZE = to_analyze
-      NUM_TO_SHOW = num_to_show
-      API_KEY = st.secrets.key if st.session_state.testKey else st.session_state.key
+      num_links_to_parse = to_parse
+      num_links_to_analyze = to_analyze
+      num_items_to_show = to_show
+
       st_progress_bar = st.progress(0)
 
-      username_dict, hashtag_dict, pfp_dict, gemini_output = on_upload(zip_file, selection, st_progress_bar=st_progress_bar)
+      username_dict, hashtag_dict, pfp_dict, gemini_response = on_upload(
+        zip_file,
+        selection,
+        st_progress_bar=st_progress_bar)
 
       st.divider()
       st.header("**💬 Your most frequent hashtags and users:**")
 
-      hashtags_to_show = list(hashtag_dict.keys())[:NUM_TO_SHOW]
-      hashtag_counts_to_show = [str(hashtag_dict[hashtag]) for hashtag in hashtags_to_show]
+      hashtags_to_show = list(hashtag_dict.keys())[:num_items_to_show]
+      hashtag_counts_to_show = [str(hashtag_dict[hashtag]) for hashtag in
+                                hashtags_to_show]
 
       df1 = pd.DataFrame(
           list(zip(hashtags_to_show, hashtag_counts_to_show)),
           columns=["#", "Number of videos"]
       )
 
-      users_to_show = list(username_dict.keys())[:NUM_TO_SHOW]
+      users_to_show = list(username_dict.keys())[:num_items_to_show]
       avatars_to_show = [pfp_dict.get(user, "") for user in users_to_show]
-      num_videos_to_show = [str(username_dict[user]) for user in users_to_show]
-      links_to_show = ["https://www.tiktok.com/" + user for user in users_to_show]
+      numvideos_to_show = [str(username_dict[user]) for user in users_to_show]
+      links_to_show = ["https://www.tiktok.com/" + user for user in
+                       users_to_show]
 
       df2 = pd.DataFrame(
-          list(zip(avatars_to_show, users_to_show, num_videos_to_show, links_to_show)),
+          list(zip(avatars_to_show, users_to_show, numvideos_to_show,
+                   links_to_show)),
           columns=["Avatar", "Username", "# of Videos", "Link"]
       )
 
       st.dataframe(df1, hide_index=True, use_container_width=True)
-      st.dataframe(df2, hide_index=True, column_config={"Link": st.column_config.LinkColumn("Link"),
-                                                        "Avatar": st.column_config.ImageColumn()},
+      st.dataframe(df2, hide_index=True,
+                   column_config={"Link": st.column_config.LinkColumn("Link"),
+                                  "Avatar": st.column_config.ImageColumn()},
                    use_container_width=True)
 
-      if gemini_output.startswith("Error"):
-        st.error(f'**👾 There was an unexpected error generating your AI analysis. Check the "Additional Help" section for more info:**\n\n:red[`{gemini_output}`]')
+      if isinstance(gemini_response, Exception):
+        st.error(
+            f'**👾 There was an unexpected error generating your AI analysis. Check the "Additional Help" section for more info:**\n\n:red[`{str(gemini_response)}`]')
       else:
         st.header("**🖨️ Your AI analysis:**")
-        st.write_stream(stream_data(gemini_output))
+        st.write_stream(stream_data(gemini_response))
